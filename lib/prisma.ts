89c9940e4f69c14ce8f12d46619bbe903581d141
Mahesh K S <mahesh25242@gmail.com>
2025-03-26 15:35:24 +0530

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,13 @@ import { PrismaClient } from '../app/generated/prisma'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+  globalForPrisma.prisma || new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + '&connection_limit=5&pool_timeout=0'
+      }
+    }
+  }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
